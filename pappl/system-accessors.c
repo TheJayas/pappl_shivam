fc92222dd0cc9af65e8f27e61fef1e2705ddacaa
TheJayas <shivam181106@gmail.com>
2024-03-04 13:05:54 +0530

    ssssssss

--- pappl/system-accessors.c
+++ pappl/system-accessors.c
@@ -1941,14 +1941,13 @@ papplSystemSetLogLevel(
 {
   if (system)
   {
-    // pthread_mutex_lock(&system->log_mutex);
-    _papplRWLockWrite(system);
+    pthread_mutex_lock(&system->log_mutex);
 
     system->log_level = loglevel;
-    _papplRWUnlock(system);
+
     _papplSystemConfigChanged(system);
 
-    // pthread_mutex_unlock(&system->log_mutex);
+    pthread_mutex_unlock(&system->log_mutex);
   }
 }
 
--- pappl/system-private.h
+++ pappl/system-private.h
@@ -78,6 +78,7 @@ struct _pappl_system_s			// System data
   char			*log_file;		// Log filename, if any
   int			log_fd;			// Log file descriptor, if any
   pappl_loglevel_t	log_level;		// Log level
+  cups_array_t		*log_array;
   size_t		log_max_size;		// Maximum log file size or `0` for none
   bool			log_is_syslog;		// Log to system log?
   char			*subtypes;		// DNS-SD sub-types, if any
