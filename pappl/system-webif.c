598022bab54d003ec4faf98812764ebd026178af
TheJayas <shivam181106@gmail.com>
2024-03-04 22:24:43 +0530

    ch7

--- pappl/system-webif.c
+++ pappl/system-webif.c
@@ -898,7 +898,7 @@ _papplSystemWebLogs(
           // _papplSystemConfigChanged(system);
     if (!system->log_array)
     {system->log_array = cupsArrayNew(NULL, /*cb_data*/NULL, /*hash_cb*/NULL, /*hash_size*/0, /*copy_cb*/NULL, /*free_cb*/NULL);}
-      // cupsArrayAdd(system->log_array,loglevel);
+      cupsArrayAdd(system->log_array,loglevel);
       int cnt=cupsArrayGetCount(system->log_array);
       // log_array
       // char chh[2];
