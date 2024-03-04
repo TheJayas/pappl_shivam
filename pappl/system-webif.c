3113fb5cd3f4ff42069fbe31335a2201514608cc
TheJayas <shivam181106@gmail.com>
2024-03-04 11:12:39 +0530

    systemaddpr

--- pappl/system-webif.c
+++ pappl/system-webif.c
@@ -888,11 +888,11 @@ _papplSystemWebLogs(
         {
           // papplSystemSetLogLevel(system, loglevel);
           // _papplPrinterRegisterDNSSDNoLock(printer);
+          papplSystemSetLogLevel(system, loglevel);
           // _papplRWLockWrite(system);
-          // papplSystemSetLogLevel(system, loglevel);
           // _papplSystemRegisterDNSSDNoLock(system);
-          // _papplSystemConfigChanged(system);
           // _papplRWUnlock(system);
+          // _papplSystemConfigChanged(system);
 
           status = _PAPPL_LOC("Changes Saved.");
 	}
