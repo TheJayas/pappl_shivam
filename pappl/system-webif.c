870a2b606889e7c4d7bb75559283832af088a8ff
TheJayas <shivam181106@gmail.com>
2024-03-10 00:30:15 +0530

    tp9

--- pappl/system-webif.c
+++ pappl/system-webif.c
@@ -722,7 +722,9 @@ _papplSystemWebHome(
               "        </div>\n"
                           "        <div class=\"col-6\">\n"
                           "          <h1 class=\"title\">%s</h1>\n", papplClientGetLocString(client, _PAPPL_LOC("Test Job Queue")));
-    // papplClientHTMLStartForm(client, uri, false);
+    char uri[256];
+    snprintf(uri, sizeof(uri), "%s/", "TEST PRINTER");
+    papplClientHTMLStartForm(client, uri, false);
     papplClientHTMLPrintf(client, "<input type=\"hidden\" name=\"action\" value=\"print-test-page\"><input type=\"submit\" value=\"%s\"></form>", papplClientGetLocString(client, _PAPPL_LOC("Print Testt Page")));
   }
   papplClientHTMLPuts(client,
