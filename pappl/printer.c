dbdf1d7cb4be32f5dd62838bbb7f788da0bb5d17
TheJayas <shivam181106@gmail.com>
2024-03-10 00:59:54 +0530

    tp11

--- pappl/printer.c
+++ pappl/printer.c
@@ -102,8 +102,9 @@ papplPrinterCreate(
     const char           *printer_name,	// I - Human-readable printer name
     const char           *driver_name,	// I - Driver name
     const char           *device_id,	// I - IEEE-1284 device ID
-    const char           *device_uri,bool tsp=false)	// I - Device URI
+    const char           *device_uri)	// I - Device URI
 {
+  bool tsp=false;
   pappl_printer_t	*printer;	// Printer
   char			resource[1024],	// Resource path
 			*resptr,	// Pointer into resource path
